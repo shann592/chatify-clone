4fff31d3ddc32a7c6713f373cb6fe16bbac7dab6
Shantanu <shannformal323@gmail.com>
2026-03-25 21:32:30 +0530

    chore: ready for deployment

--- backend/src/main.js
+++ backend/src/main.js
@@ -1,8 +1,11 @@
 import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
+import path from "path";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -10,4 +13,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 const PORT = process.env.PORT || 5000;
+
+//make ready for deployment as single app
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get(/.*/, (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
